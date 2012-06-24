1ea887dcdb99550171ea064063ab42e4dad80ae4
kazuhiro homma <kazu.homma@gmail.com>
2012-06-24 19:22:56 -0400

    第2章おわり！

--- section_2/code2.rb
+++ section_2/code2.rb
@@ -19,3 +19,25 @@ p sym
 p sym2
 p sym.to_s # 文字に変換
 p sym.to_s.intern # 文字に変換後更にシンボルに変換
+
+# Symbolクラスってのがあるみたい
+p sym.class
+p sym2.class
+p sym.class.class
+p sym2.class.class
+
+font_table = {:normal => "+0", :small => "-1", :big => "+1"}
+p font_table
+
+p font_table[:small]
+p font_table[:normal]
+p font_table[:big]
+p font_table[:verybig] = 2
+
+print "<html><title>font size list</title>"
+print "<body>\n<p>\n"
+font_table = {:normal => "+0", :small => "-1", :big => "+1"}
+font_table.each do |key, value|
+    print '<font size="', value, '">', key, '</font><br>', "\n"
+end
+print "</p></body></html>\n"
